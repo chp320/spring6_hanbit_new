bb85527f25d51ddcdaf2739d2ac74012063b6817
chp320 <chp320@naver.com>
2024-06-02 20:36:01 +0900

    의존성 주입 테스트 코드 작성

---
+++ src/test/java/com/training/train_0301_di/MockTrainingRepository.java
@@ -0,0 +1,21 @@
+package com.training.train_0301_di;
+
+import com.training.train_0001_common.entity.Training;
+import com.training.train_0301_di.repository.TrainingRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MockTrainingRepository implements TrainingRepository {
+    @Override
+    public List<Training> selectAll() {
+        System.out.println("[MockTrainingRepository] 테스트용 데이터를 메모리상에 준비합니다.");
+        List<Training> trainings = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Training training = new Training();
+            training.setTitle("title_" + i);
+            trainings.add(training);
+        }
+        return trainings;
+    }
+}
---
+++ src/test/java/com/training/train_0301_di/TrainingServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.training.train_0301_di;
+
+import com.training.train_0001_common.entity.Training;
+import com.training.train_0301_di.repository.TrainingRepository;
+import com.training.train_0301_di.service.TrainingService;
+import com.training.train_0301_di.service.TrainingServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TrainingServiceImplTest {
+
+    @Test
+    @DisplayName("의존객체를_Mock객체로_변경하여도_의존성주입은_정상_처리된다.")
+    public void test_findAll() {
+        TrainingRepository trainingRepository = new MockTrainingRepository();
+        TrainingService trainingService = new TrainingServiceImpl(trainingRepository);
+
+        List<Training> trainings = trainingService.findAll();
+
+        // 결과 확인
+        assertThat(trainings.size()).isEqualTo(10);
+    }
+}
